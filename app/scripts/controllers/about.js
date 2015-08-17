03a231e875421127400780ff437fe60433ddf218
Eric Foard <foard265@gmail.com>
2015-08-17 11:56:28 -0700

    first commit, yeoman scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvfmApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pvfmApp
+ */
+angular.module('pvfmApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
