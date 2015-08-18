1217c4414ca4e469a7478e19621eddb0f2272568
Eric Foard <foard265@gmail.com>
2015-08-17 23:04:19 -0700

    css updates and finished photo carosel

---
+++ app/scripts/controllers/photo.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvfmApp.controller:PhotoCtrl
+ * @description
+ * # PhotoCtrl
+ * Controller of the pvfmApp
+ */
+ angular.module('pvfmApp')
+ .controller('PhotoCtrl', function ($scope) {
+
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.slides = [
+    {image: 'images/coffee.png'},
+    {image: 'images/carrots.png'},
+    {image: 'images/glamor-bread.png'},{image: 'images/happy.png'},
+    {image: 'images/peppers.png'},{image: 'images/strawberries.png'},
+    {image: 'images/walnut.png'},{image: 'images/pops.png'}];
+
+ });
