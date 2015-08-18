7576f4f1fb77a4393f1376c8f9d3e3a50f582222
Eric Foard <foard265@gmail.com>
2015-08-17 20:11:56 -0700

    updated leaflet markers and centered on market location

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -11,16 +11,25 @@
  .controller('MapCtrl', function ($scope) {
 
     $scope.center = {
-            lat: 37.382468,
+        lat: 37.382468,
         lng: -122.228823,
-            zoom: 12,
-        };
+        zoom: 12,
+    };
 
     $scope.markers = { 
         marker: {
-        lat: 37.382468,
-        lng: -122.228823,
-        focus: true
-    }};
+            lat: 37.382468,
+            lng: -122.228823,
+            focus: true,
+            icon: {
+                iconUrl: 'images/leaf-green.png',
+                shadowUrl: 'images/leaf-shadow.png',
+                    iconSize:     [38, 95], // size of the icon
+                    shadowSize:   [50, 64], // size of the shadow
+                    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                    shadowAnchor: [4, 62],  // the same for the shadow
+                    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+                }
+            }};
 
- });
+        });
